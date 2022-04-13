093bf85c4fb6f2400a088bafa747ef671a43ac08
KellySchaefer625 <62809689+KellySchaefer625@users.noreply.github.com>
2022-04-13 09:55:27 -0400

    Update addEventPage.php

--- addEventPage.php
+++ addEventPage.php
@@ -87,9 +87,9 @@
     </div> 
 
   <div class="row mb-3 mx-3">
+  <button class="btn btn-outline-secondary" type="button">+</button>
     Event Categories: 
     <div class="input-group-append">
-    <button class="btn btn-outline-secondary" type="button">+</button>
     </div>
        <input type="text" class="form-control" name="categories" />
   </div>
