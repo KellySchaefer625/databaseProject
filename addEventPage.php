37828455ddd8a217d31d68041f81bc8adff4139d
KellySchaefer625 <62809689+KellySchaefer625@users.noreply.github.com>
2022-04-14 19:36:23 -0400

    Update addEventPage.php

--- addEventPage.php
+++ addEventPage.php
@@ -163,7 +163,16 @@
         <input type="text" class="form-control" name="restrictions" required />
  </div>
 
-
+<div class="container">
+  <h2>Simple Collapsible</h2>
+  <p>Click on the button to toggle between showing and hiding content.</p>
+  <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Simple collapsible</button>
+  <div id="demo" class="collapse">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  </div>
+</div>
 
     <input type="submit" value="Add" name="btnAction" class="btn btn-dark"
 
