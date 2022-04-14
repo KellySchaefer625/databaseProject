0b85cd971b675a0baf5b9b32f43a381563d778df
KellySchaefer625 <62809689+KellySchaefer625@users.noreply.github.com>
2022-04-14 09:47:13 -0400

    Update addEventPage.php

--- addEventPage.php
+++ addEventPage.php
@@ -155,9 +155,20 @@
    <button class="btn btn-outline-secondary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" value="HostAdd" type="button">+</button>
   </div>
  
-  <div id="collapseExample">
-   <input type="text" class="form-control" name="restrictions2" required />
+ <p>
+  <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Link with href
+  </a>
+  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Button with data-target
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
   </div>
+</div>
+
 
     <input type="submit" value="Add" name="btnAction" class="btn btn-dark"
 
