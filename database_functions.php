8af6d1c26f5a87526302aeb9e2b3a18f8307cf38
Fiji Marcelin <fiji4real@gmail.com>
2022-04-14 19:41:04 -0400

    adding login functionality

--- database_functions.php
+++ database_functions.php
@@ -547,5 +547,50 @@ function getEventRestrictions($event_id)
         throw new Exception('Error getting restrictions');
     }
 }
+//TODO: make get username and get password function
+function getUserCredentials($userName,$userpassword)
+{ //not finished
+    //HASH THE PASSWORDS FIJI DONT FORGET, YA STUPID!
+    try{
+    global $db;
+    //Prep SQL query
+    $query = " SELECT comp_id, userpassword FROM User_by_id WHERE comp_id = ?";
+
+    $statement = $db->prepare($query); //Prep statement and bind Values
+    $statement->bindValue(':comp_id',$userName);
+    $statement->bindValue(':password',$userpassword);
+
+    $paramUsername = trim($_POST["comp_id"]);
+    if($statement->execute()){
+        if($statement->rowCount()==1){
+            $userData = $statement->fetch();
+            $userName = $userData["comp_id"];
+            //uncomment after hashing passwords
+            $hashPass = $userData["password"];
+            if(password_verify($userpassword,$hashPass)){
+                //if correct, return all data, but send password hashed
+                $statement->closeCursor();    
+                return $userData;
+            }
+            else{
+                $statement->closeCursor();    
+                return -1;//"invalid user or pass"
+            }
+
+            }
+        }
+        else{
+            $statement->closeCursor();    
+            return -3;//"server error"
+        }
+    }
+
+    
+    //$results = $statement->fetch();
+    
+    catch(Exception $execpt){
+        throw new Exception('Error getting userName and passWord');
+    }
+}
 
 ?>
