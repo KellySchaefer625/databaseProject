525150d4821861f69ee5aca036dc89ed79bcb7b5
KellySchaefer625 <62809689+KellySchaefer625@users.noreply.github.com>
2022-04-14 19:28:23 -0400

    Update addEventPage.php

--- addEventPage.php
+++ addEventPage.php
@@ -90,10 +90,14 @@
         <input type="text" class="form-control" name="org_name" required />        
   </div> 
  
-  <div class="row mb-3 mx-3"> 
-   <button class="btn btn-outline-secondary" data-toggle="collapse" data-target="#org_name2" value="HostAdd" type="button">+</button>
+  <div class="row mb-3 mx-3">
+   <div class="collapse">
+    <button class="btn btn-outline-secondary" data-toggle="collapse" data-target="#org_name2" value="HostAdd" type="button">+</button>
+   </div
   </div>
+   
  
+  
   <div class="row mb-3 mx-3">
         <input type="text" class="form-control" name="org_name2"/>        
   </div> 
