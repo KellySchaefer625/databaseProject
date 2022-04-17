b52f02f080c4286670fe1166fe0a55f24451f1b5
jaredtewodros <jmt5rg@virginia.edu>
2022-04-17 19:31:35 -0400

    reworking file upload - file is not added to  var

--- database_functions.php
+++ database_functions.php
@@ -11,7 +11,7 @@ function getLatestEventId()
     
     $statement->execute();
     
-    $results = $statement->fetch();
+    $results = $statement->fetch()[0];
 
     $statement->closeCursor();
     
@@ -30,6 +30,36 @@ function addToEvent_By_ID($name, $time_start, $time_end, $building, $room, $date
     // keyword global allows us to access db in connect-db
     try{
     global $db;
+
+    print "<pre>";
+    print_r ($name);
+    print_r (gettype($name));
+    print_r ($time_start);
+    print_r (gettype($time_start));
+    print_r ($time_end);
+    print_r (gettype($time_end));
+    print_r ($building);
+    print_r (gettype($building));
+    print_r ($room);
+    print_r (gettype($room));
+    print_r ($date_of_event);
+    print_r (gettype($date_of_event));
+    print_r ($cost);
+    print_r (gettype($cost));
+    print_r ($food);
+    // print_r (gettype($food));
+    // print_r ($org_name);
+    // print_r (gettype($org_name));
+    // print_r ($audience);
+    // print_r (gettype($audience));
+    // print_r ($categories);
+    // print_r (gettype($categories));
+    // print_r ($restrictions);
+    // print_r (gettype($restrictions));
+    // print_r ($latest_event_id);
+    // print_r (gettype($latest_event_id));
+    print "</pre>";
+
 
     //sql
     $query = "INSERT INTO Event_by_id (name, time_start, time_end, building, room, date_of_event, cost, food) VALUES (:name, :time_start, :time_end, :building, :room, :date_of_event, :cost, :food)";
